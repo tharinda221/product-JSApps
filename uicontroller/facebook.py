9e7a0bcac94e091f52b0040d3684cf603ace6c94
tharinda <tharinda221@gmail.com>
2016-06-03 21:48:23 +0530

    logs added

--- uicontroller/facebook.py
+++ uicontroller/facebook.py
@@ -45,6 +45,7 @@ class facebookAuthorized(Resource):
 
 class facebook(Resource):
     def get(self):
+        logging.info("user Entered")
         global noOfAppsPagesFacebook, facebookUserObj, facebookAppCount, FacebookAppList
         facebookUserObj = getFacebookUser()
         startId, endId = getStartIdAndEndId(1, facebookAppCount)
