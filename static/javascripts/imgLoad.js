06c8a29fc80256e3724131c285f9863db2f2a702
tharinda <tharinda221@gmail.com>
2016-08-03 21:47:31 +0530

    loading gif changed

--- static/javascripts/imgLoad.js
+++ static/javascripts/imgLoad.js
@@ -2,6 +2,7 @@
  * Created by tharinda on 8/2/16.
  */
 $('#fb-logging').click(function () {
-    $('#app-init-image').attr('src', 'http://i.imgur.com/8YsAmq3.gif');
+    // $('#app-init-image').attr('src', 'http://i.imgur.com/8YsAmq3.gif');
+    $('#app-init-image').attr('src', 'http://9to5animations.com/wp-content/uploads/2016/03/loading-gif.gif');
     $('#fb-logging').hide();
 });
